1f9d4dc6973054e81cc78d19130e0812cebd8613
Szymon Wnuk <101000022+HelloSzymon@users.noreply.github.com>
2023-07-27 12:21:37 +0200

    Initial Commit

---
+++ Magnifier Effect/Magnifier_EffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magnifier_EffectApp.swift
+//  Magnifier Effect
+//
+//  Created by Szymon Wnuk on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Magnifier_EffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
